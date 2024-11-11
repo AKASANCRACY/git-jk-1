b5ea072f3ca8e9c137a906264dc0c42385f79561
Prakash Subramanian <64981127+AKASANCRACY@users.noreply.github.com>
2024-11-11 10:26:45 +0530

    Update Test.java
    I just changes some code

--- Test.java
+++ Test.java
@@ -3,6 +3,6 @@ public class Test
     public static void main(String []args)
     {
         for(int i=1;i<=5;i++)
-        System.out.println("Hi hello I am Prakash.");
+        System.out.println("This is my first Java test program.");
     }
 };
